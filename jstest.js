8f0153c7f02653ca4499fe4d571e808f7c41bbdc
Ohelig <zorum50@gmail.com>
2013-11-03 14:55:16 -0800

    aaaaaa

--- jstest.js
+++ jstest.js
@@ -3,6 +3,7 @@ API.moderateSetRole("50b901253e083e29cd35afb8", API.ROLE.FEATUREDDJ)
 API.moderateSetRole("521ee701877b92544906d9d3", API.ROLE.COHOST)
 while (true)
   {
-  API.sendChat("THE FUSIONFOX GOES 666666666666")
-  API.sendChat("FOR I AM FUSIONFOX")
+  API.sendChat("THE FOX GOES 666")
+  API.sendChat("FOR I AM THE FOX")
+  API.sendChat("FEAR THE ARRIVAL")
   }
