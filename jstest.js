c75eaa27cfc48447f2d0660d2d2ec87e6501efbe
Ohelig <zorum50@gmail.com>
2013-11-03 15:12:45 -0800

    .

--- jstest.js
+++ jstest.js
@@ -1,9 +1,3 @@
 ﻿API.moderateSetRole("5169a3763b7903388503accf", API.ROLE.FEATUREDDJ)
 API.moderateSetRole("50b901253e083e29cd35afb8", API.ROLE.FEATUREDDJ)
 API.moderateSetRole("521ee701877b92544906d9d3", API.ROLE.MANAGER)
-while (true)
-  {
-  API.sendChat("THE FOX GOES 666")
-  API.sendChat("FOR I AM THE FOX")
-  API.sendChat("FEAR THE ARRIVAL")
-  }
