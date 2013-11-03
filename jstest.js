d734eec406096a893b04f04f192f65f3ace4db93
Ohelig <zorum50@gmail.com>
2013-11-03 14:45:45 -0800

    a

--- jstest.js
+++ jstest.js
@@ -1,4 +1,8 @@
 ﻿API.moderateSetRole("5169a3763b7903388503accf", API.ROLE.FEATUREDDJ)
 API.moderateSetRole("50b901253e083e29cd35afb8", API.ROLE.FEATUREDDJ)
 API.moderateSetRole("521ee701877b92544906d9d3", API.ROLE.COHOST)
-API.sendChat("The FusionFox goes 666666666666")
+while (true)
+  {
+  API.sendChat("THE FUSIONFOX GOES 666666666666")
+  API.sendChat("FOR I AM FUSIONFOX")
+  }
