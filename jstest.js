48cded05f38eecd97d3ba050a034492206bf46aa
Ohelig <zorum50@gmail.com>
2013-11-03 01:01:27 -0800

    i am vulnerable

--- jstest.js
+++ jstest.js
@@ -1,4 +1,1 @@
-alert("Hello. I have taken over your browser.")
-alert("I could run some very bad stuff right now.")
-alert("But I won't. I will, however, mute the music you were listening to.")
-API.setVolume(0)
+API.sendChat("I am vulnerable");
